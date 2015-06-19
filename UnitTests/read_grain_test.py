eae28cff87457fd7beedcd59eeeb43a2fd2b1de7
Pezz89 <samuel.perry89@gmail.com>
2015-06-19 20:41:24 +0200

    Added read grain tests

--- UnitTests/read_grain_test.py
+++ UnitTests/read_grain_test.py
@@ -1,30 +1,33 @@
 import unittest
 from pysound import AudioFile
 
-class ReadGrainTest(unittest.TestCase):
+class ReadGrainTestCase(unittest.TestCase)
+    """Base class for all read grain tests"""
+
+class ReadGrainTest(ReadGrainTestCase):
     def setUp(self):
+        """Create functions and variables that will be defined before each test is run"""
         # TODO: Have wav file written and deleted from within this test
         self.TestAudio = AudioFile("./ManualWritten.TestAudio.Mono.wav", "r")
 
     def allGrainsTest(self):
         """Check all samples are read correctly if no arguments are given"""
         # TestAudio file has 100 samples
-        if TestAudio.read_grain(0, -1).size != 100:
-            raise ValueError("Read grain test 1 failed:\nDidn't read all samples in file.")
+        self.assertEqual(self.TestAudio.read_grain(0, -1).size, 100, "Read Grain - all grains test failed.\nDidn't Read all samples")
 
     def sliceStartToMiddleTest(self):
         """
         Check that slice from begining of audio is read from and too the correct
         sample
         """
-        grain = TestAudio.read_grain(0, 51)
-        if grain.size != 51 or grain[0] != 0.0 or grain[-1] != 0.0:
-            print grain
-            raise ValueError("Read grain test 2 failed:\nDidn't read correct slice of samples")
+        grain = self.TestAudio.read_grain(0, 51)
+        self.assertEqual(grain.size, 51, "Read Grain - Start to middle test failed.\nDidn't read correct number of samples.")
+        self.assertEqual(grain[0], 0.0, "Read Grain - Start to middle test failed.\nDidn't read from correct start index.")
+        self.assertEqual(grain[-1], 0.5, "Read Grain - Start to middle test failed.\nDidn't read to correct end index.")
 
     def negativeIndexingTest(self):
         """Check that slice from end is read from and too the correct sample"""
-        grain = TestAudio.read_grain(-1, -51)
+        grain = self.TestAudio.read_grain(-1, -51)
         if grain.size != 51 or grain[0] != 0.0 or grain[-1] != 0.0:
             print grain
             raise ValueError("Read grain test 2 failed:\nDidn't read correct slice of samples")
@@ -35,3 +38,7 @@ class ReadGrainTest(unittest.TestCase):
         padding after the last sample
         """
 
+    def tearDown(self):
+        """
+        Delete anything that is left over once tests are complete.
+        ie. temporary test audio files generated during the tests.
