db2102df0080b5f7127a8f864323f30a66037927
Sam Perry <samuel.perry89@gmail.com>
2015-10-22 00:43:43 +0100

    Begun context manager implementation of AudioFile object

--- src/sppysound/create_database.py
+++ src/sppysound/create_database.py
@@ -7,6 +7,29 @@ import audiofile
 
 
 def main():
+    import __builtin__
+    openfiles = set()
+    oldfile = __builtin__.file
+    class newfile(oldfile):
+        def __init__(self, *args):
+            self.x = args[0]
+            print "### OPENING %s ###" % str(self.x)
+            oldfile.__init__(self, *args)
+            openfiles.add(self)
+
+        def close(self):
+            print "### CLOSING %s ###" % str(self.x)
+            oldfile.close(self)
+            openfiles.remove(self)
+    oldopen = __builtin__.open
+    def newopen(*args):
+        return newfile(*args)
+    __builtin__.file = newfile
+    __builtin__.open = newopen
+
+    def printOpenFiles():
+        print "### %d OPEN FILES: [%s]" % (len(openfiles), ", ".join(f.x for f in openfiles))
+
     """Parse arguments then generate database."""
     parser = argparse.ArgumentParser(
         description='Generate a database at argument 1 based on files in '
