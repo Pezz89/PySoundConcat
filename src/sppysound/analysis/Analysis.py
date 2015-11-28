43736de15377711347bfd07b4eaa64aa9b9bd3c1
Pezz89 <samuel.perry89@gmail.com>
2015-11-28 15:00:31 +0000

    Implemented analysis class inheritance for Zero crossing

---
+++ src/sppysound/analysis/Analysis.py
@@ -0,0 +1,62 @@
+from __future__ import print_function
+import os
+import numpy as np
+import logging
+import pdb
+
+from fileops import pathops
+
+logger = logging.getLogger(__name__)
+
+class Analysis(object):
+
+    """Basic descriptor class to build analyses on."""
+
+    def __init__(self, AnalysedAudioFile, analysis_group, name):
+        # Create object logger
+        self.logger = logging.getLogger(__name__ + '.{0}Analysis'.format(name))
+        # Store AnalysedAudioFile object to be analysed.
+        self.AnalysedAudioFile = AnalysedAudioFile
+        self.analysis_group = analysis_group
+        self.name = name
+
+    def create_analysis(self, analysis_function, *args, **kwargs):
+        """
+        Create the analysis and save to the HDF5 file.
+
+        analysis_function: The function used to create the analysis. returned
+        data will be stored in the HDF5 file.
+        """
+
+        try:
+            self.analysis_data = self.analysis_group.create_group(self.name)
+        except ValueError:
+            self.logger.warning("{0} analysis group already exists".format(self.name))
+            self.analysis_data = self.analysis_group[self.name]
+
+        # If forcing new analysis creation then delete old analysis and create
+        # a new one
+        if self.AnalysedAudioFile.force_analysis:
+            self.logger.warning("Force re-analysis is enabled. "
+                                "deleting: {0}".format(self.analysis_data.name))
+            # Delete all pre-existing data in database.
+            for i in self.analysis_data.iterkeys():
+                del self.analysis_data[i]
+            self.analysis = analysis_function(args, kwargs)
+        else:
+            # Check if analysis file already exists.
+            try:
+                self.analysis = self.analysis_data['data']
+                self.logger.info("Analysis already exists. "
+                                 "Reading from: {0}".format(self.analysis_data.name))
+                # If an analysis file is provided then count the number of lines
+                # (1 for each window)
+                self.logger.info(''.join(("Reading {0} data: "
+                                          "(HDF5 File)".format(self.name),
+                                          self.analysis_data.name)))
+                self.window_count = self.analysis.size
+                self.logger.debug(''.join(("{0} Window Count: ".format(self.name),
+                                           str(self.window_count))))
+            except KeyError:
+                # If it doesn't then generate a new file
+                self.analysis = analysis_function(args, kwargs)
--- src/sppysound/analysis/ZeroXAnalysis.py
+++ src/sppysound/analysis/ZeroXAnalysis.py
@@ -2,57 +2,32 @@ from __future__ import print_function
 import os
 import numpy as np
 import logging
+from Analysis import Analysis
+import pdb
 
 from fileops import pathops
 
+logger = logging.getLogger(__name__)
 
-class ZeroXAnalysis:
+class ZeroXAnalysis(Analysis):
 
     """ """
 
     def __init__(self, AnalysedAudioFile, analysis_group):
-        self.logger = logging.getLogger(__name__ + '.ZeroXAnalysis')
-        self.AnalysedAudioFile = AnalysedAudioFile
+        super(ZeroXAnalysis, self).__init__(AnalysedAudioFile, analysis_group, 'name')
+        self.logger = logging.getLogger('audiofile.{0}'.format(self.name))
         self.zerox_window_count = None
+        self.logger.debug(''.join(("zero-crossing Window Count: ",
+                                    str(self.zerox_window_count))))
+        self.analysis_group = analysis_group
+        self.create_analysis(self.create_zerox_analysis)
 
-        # Store the path to the FFT file if it already exists
-        try:
-            self.analysis_data = analysis_group.create_group('zerox')
-        except ValueError:
-            self.logger.warning("Zero-crossing analysis group already exists")
-            self.analysis_data = analysis_group['zerox']
-
-        # If forcing new analysis creation then delete old analysis and create
-        # a new one
-        if self.AnalysedAudioFile.force_analysis:
-            self.logger.warning("Force re-analysis is enabled. "
-                                "deleting: {0}".format(self.analysis_data.name))
-            # Delete all pre-existing data in database.
-            for i in self.analysis_data.iterkeys():
-                del self.analysis_data[i]
-            self.zerox_analysis = self.create_zerox_analysis()
-        else:
-            # Check if analysis file already exists.
-            try:
-                self.zerox_analysis = self.analysis_data['data']
-                self.logger.info("Analysis already exists. "
-                                 "Reading from: {0}".format(self.analysis_data.name))
-                # If an zero-crossing file is provided then count the number of lines
-                # (1 for each window)
-                self.logger.info(''.join(("Reading zero-crossing data: "
-                                          "(HDF5 File)",
-                                          self.analysis_data.name)))
-                self.zerox_window_count = self.zerox_analysis.size
-                self.logger.debug(''.join(("zero-crossing Window Count: ",
-                                           str(self.zerox_window_count))))
-            except KeyError:
-                # If it doesn't then generate a new file
-                self.zerox_analysis = self.create_zerox_analysis()
+        self.zerox_window_count = self.analysis.size
 
 
-    def create_zerox_analysis(self):
+    def create_zerox_analysis(self, *args, **kwargs):
         """Generate zero crossing detections for windows of the signal"""
         zero_crossings = np.where(np.diff(np.sign(
-            self.AnalysedAudioFile.read_grain())))
+            self.AnalysedAudioFile.read_grain())))[0]
         self.analysis_data.create_dataset('data', data=zero_crossings)
         return zero_crossings
