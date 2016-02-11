Fixed frame time calculation of multiple analyses. finished initial difference array calculations in matcher. array is currently not normalized.

src/sppysound/analysis/Analysis.py
@@ -86,19 +86,20 @@ class Analysis(object):
 
     def mean_formatter(self, data):
         """Calculate the mean value of the analysis data"""
-        if not data.size:
-            return np.array([])
-        output = np.empty(len(data))
-        for ind, i in enumerate(data):
+
+        values = data[0]
+
+        output = np.empty(len(values))
+        for ind, i in enumerate(values):
             output[ind] = np.mean(i)
         return output
 
     def median_formatter(self, data):
         """Calculate the median value of the analysis data"""
-        if not data.size:
-            return np.array([])
+        values = data[0]
+
         output = np.empty(len(data))
-        for ind, i in enumerate(data):
+        for ind, i in enumerate(values):
             output[ind] = np.median(i)
         return output
 

src/sppysound/analysis/RMSAnalysis.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+from __future__ import print_function, division
 import os
 import numpy as np
 import logging
@@ -94,11 +94,10 @@ class RMSAnalysis(Analysis):
 
         selection = np.transpose((vtimes >= start) & (vtimes <= end))
 
-        np.set_printoptions(threshold=np.nan)
-
-        grain_data = []
+        grain_data = [[],[]]
         for grain in selection:
-            grain_data.append((self.analysis_group["RMS"]["frames"][grain], times[grain]))
+            grain_data[0].append(self.analysis_group["RMS"]["frames"][grain])
+            grain_data[1].append(times[grain])
 
         return grain_data
 
@@ -122,7 +121,7 @@ class RMSAnalysis(Analysis):
         scale = np.arange(timebins+1)
         # divide the number of samples by the total number of frames, then
         # multiply by the frame numbers.
-        rms_times = (sample_frames.shape[0]/timebins) * scale[:-1]
+        rms_times = (float(sample_frames.shape[0])/float(timebins)) * scale[:-1].astype(float)
         # Divide by the samplerate to give times in seconds
         rms_times = rms_times / samplerate
         return rms_times

src/sppysound/analysis/SpectralCentroidAnalysis.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+from __future__ import print_function, division
 import numpy as np
 import logging
 import pdb
@@ -45,11 +45,10 @@ class SpectralCentroidAnalysis(Analysis):
 
         selection = np.transpose((vtimes >= start) & (vtimes <= end))
 
-        np.set_printoptions(threshold=np.nan)
-
-        grain_data = []
+        grain_data = [[],[]]
         for grain in selection:
-            grain_data.append((self.analysis_group["SpcCntr"]["frames"][grain], times[grain]))
+            grain_data[0].append(self.analysis_group["SpcCntr"]["frames"][grain])
+            grain_data[1].append(times[grain])
 
         return grain_data
 
@@ -59,7 +58,7 @@ class SpectralCentroidAnalysis(Analysis):
         '''
         samplerate = self.AnalysedAudioFile.samplerate
         output = self.create_spccntr_analysis(*args, **kwargs)
-        times = self.calc_spccntr_frame_times(output, args[0], samplerate)
+        times = self.calc_spccntr_frame_times(output, self.AnalysedAudioFile.frames, samplerate)
         return ({'frames': output, 'times': times}, {})
 
     @staticmethod
@@ -84,12 +83,13 @@ class SpectralCentroidAnalysis(Analysis):
             raise ValueError("\'{0}\' is not a valid output "
                              "format.".format(output_format))
         # Calculate the weighted mean
-        y = np.sum(magnitudes*freqs, axis=1) / np.sum(magnitudes, axis=1)
+        y = np.sum(magnitudes*freqs, axis=1) / (np.sum(magnitudes, axis=1)+np.finfo(float).eps)
+
         # Convert from index to Hz
         return y
 
     @staticmethod
-    def calc_spccntr_frame_times(spccntr_frames, sample_frames, samplerate):
+    def calc_spccntr_frame_times(spccntr_frames, sample_frame_count, samplerate):
 
         """Calculate times for frames using sample size and samplerate."""
 
@@ -99,7 +99,7 @@ class SpectralCentroidAnalysis(Analysis):
         scale = np.arange(timebins+1)
         # divide the number of samples by the total number of frames, then
         # multiply by the frame numbers.
-        spccntr_times = (sample_frames.shape[0]/timebins) * scale[:-1]
+        spccntr_times = (float(sample_frame_count)/float(timebins)) * scale[:-1].astype(float)
         # Divide by the samplerate to give times in seconds
         spccntr_times = spccntr_times / samplerate
         return spccntr_times

src/sppysound/analysis/SpectralSpreadAnalysis.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+from __future__ import print_function, division
 import numpy as np
 import logging
 import pdb
@@ -49,11 +49,10 @@ class SpectralSpreadAnalysis(Analysis):
 
         selection = np.transpose((vtimes >= start) & (vtimes <= end))
 
-        np.set_printoptions(threshold=np.nan)
-
-        grain_data = []
+        grain_data = [[],[]]
         for grain in selection:
-            grain_data.append((self.analysis_group["SpcSprd"]["frames"][grain], times[grain]))
+            grain_data[0].append(self.analysis_group["SpcSprd"]["frames"][grain])
+            grain_data[1].append(times[grain])
 
         return grain_data
 
@@ -63,7 +62,7 @@ class SpectralSpreadAnalysis(Analysis):
         '''
         samplerate = self.AnalysedAudioFile.samplerate
         output = self.create_spcsprd_analysis(*args, **kwargs)
-        times = self.calc_spcsprd_frame_times(output, args[0], samplerate)
+        times = self.calc_spcsprd_frame_times(output, self.AnalysedAudioFile.frames, samplerate)
         return ({'frames': output, 'times': times}, {})
 
     @staticmethod
@@ -93,12 +92,12 @@ class SpectralSpreadAnalysis(Analysis):
         a = np.power(freqs-spectral_centroid, 2)
         mag_sqrd = np.power(magnitudes, 2)
         # Calculate the weighted mean
-        y = np.sqrt(np.sum(a*mag_sqrd, axis=1) / np.sum(mag_sqrd, axis=1))
+        y = np.sqrt(np.sum(a*mag_sqrd, axis=1) / (np.sum(mag_sqrd, axis=1)+np.finfo(float).eps))
 
         return y
 
     @staticmethod
-    def calc_spcsprd_frame_times(spcsprd_frames, sample_frames, samplerate):
+    def calc_spcsprd_frame_times(spcsprd_frames, sample_frame_count, samplerate):
 
         """Calculate times for frames using sample size and samplerate."""
 
@@ -108,7 +107,7 @@ class SpectralSpreadAnalysis(Analysis):
         scale = np.arange(timebins+1)
         # divide the number of samples by the total number of frames, then
         # multiply by the frame numbers.
-        spcsprd_times = (sample_frames.shape[0]/timebins) * scale[:-1]
+        spcsprd_times = (float(sample_frame_count)/float(timebins)) * scale[:-1].astype(float)
         # Divide by the samplerate to give times in seconds
         spcsprd_times = spcsprd_times / samplerate
         return spcsprd_times

src/sppysound/analysis/ZeroXAnalysis.py
@@ -1,6 +1,7 @@
-from __future__ import print_function
+from __future__ import print_function, division
 import numpy as np
 import logging
+from numpy.lib import stride_tricks
 from Analysis import Analysis
 import pdb
 
@@ -20,11 +21,26 @@ class ZeroXAnalysis(Analysis):
         self.create_analysis(frames)
 
     @staticmethod
-    def create_zerox_analysis(samples, *args, **kwargs):
-        """Generate zero crossing detections for windows of the signal"""
-        # TODO: window across audiofile.
-        zero_crossings = np.where(np.diff(np.sign(samples)))[0]
-        return zero_crossings
+    def create_zerox_analysis(frames, window_size=512, overlapFac=0.5, *args, **kwargs):
+        """Generate zero crossing value for window of the signal"""
+        hopSize = int(window_size - np.floor(overlapFac * window_size))
+
+        # zeros at beginning (thus center of 1st window should be for sample nr. 0)
+        samples = np.append(np.zeros(np.floor(window_size/2.0)), frames)
+
+        # cols for windowing
+        cols = np.ceil((len(samples) - window_size) / float(hopSize)) + 1
+        # zeros at end (thus samples can be fully covered by frames)
+        samples = np.append(samples, np.zeros(window_size))
+
+        frames = stride_tricks.as_strided(
+            samples,
+            shape=(cols, window_size),
+            strides=(samples.strides[0]*hopSize, samples.strides[0])
+        ).copy()
+
+        zero_crossing = (1./(2.*samples.size))*np.sum(np.abs(np.diff(np.sign(frames))), axis=1)
+        return zero_crossing
 
     @staticmethod
     def calc_zerox_frame_times(zerox_frames, sample_frames, samplerate):
@@ -55,11 +71,10 @@ class ZeroXAnalysis(Analysis):
 
         selection = np.transpose((vtimes >= start) & (vtimes <= end))
 
-        np.set_printoptions(threshold=np.nan)
-
-        grain_data = []
+        grain_data = [[],[]]
         for grain in selection:
-            grain_data.append((self.analysis_group["ZeroCrossing"]["frames"][grain], times[grain]))
+            grain_data[0].append(self.analysis_group["ZeroCrossing"]["frames"][grain])
+            grain_data[1].append(times[grain])
 
         return grain_data
 

src/sppysound/database.py
@@ -267,6 +267,8 @@ class Matcher:
         source_grain_indexes = self.count_grains(self.source_db, grain_size, overlap)
 
         for analysis in self.matcher_analyses:
+            if not analysis == 'zerox':
+                continue
             self.logger.debug("Current analysis: {0}".format(analysis))
             analysis_formatting = self.analysis_dict[analysis]
 
@@ -288,7 +290,7 @@ class Matcher:
                     # Create an array of grain times for source sample
                     source_times = source_entry.generate_grain_times(grain_size, overlap)
 
-                    self.logger.debug("Matching {0} to {1}".format(source_entry.name, source_entry.name))
+                    self.logger.debug("Matching \"{0}\" for: {1} to {2}".format(analysis, source_entry.name, target_entry.name))
 
                     # Get data for all source grains for each analysis
                     source_data = source_entry.analysis_data_grains(source_times, analysis)
@@ -297,11 +299,15 @@ class Matcher:
                     # objects match formatting function.
                     source_data = analysis_object.formatters[analysis_formatting](source_data)
 
-                    # FIX
+                    # Get the start and end array indexes allocated for the
+                    # current entry's grains.
                     start_index, end_index = source_grain_indexes[sind]
-                    pdb.set_trace()
-                    #data_distance[:,  = np.abs(np.vstack(source_data) - source_data)
-                    pdb.set_trace()
+                    # Calculate the difference between the source and target
+                    # values of each grain and add to array
+                    data_distance[:, start_index:end_index] = np.abs(np.vstack(target_data) - source_data)
+
+
+            pdb.set_trace()
 
 
 