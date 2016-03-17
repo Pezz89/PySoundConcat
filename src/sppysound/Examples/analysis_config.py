4c7458cbbc30cb29f0e411f8ed553cd57f3275c5
Sam Perry <samuel.perry89@gmail.com>
2016-03-17 22:31:29 +0000

    added ipython notebook examples

---
+++ src/sppysound/Examples/analysis_config.py
@@ -0,0 +1,19 @@
+rms = {
+    "window_size": 100,
+    "overlap": 2,
+}
+
+analysis_dict = {
+    "f0": "log2_median",
+    "rms": "mean"
+}
+
+analysis = {
+    "reanalyse": False
+}
+
+output_file = {
+    "samplerate": 44100,
+    "format": 131075,
+    "channels": 1
+}
