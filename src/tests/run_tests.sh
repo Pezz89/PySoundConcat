c9595f43c844b41fdf0059974866729fa77a0e0b
Pezz89 <samuel.perry89@gmail.com>
2016-05-11 21:16:57 +0100

    Fixed spaces error with run_tests script

--- src/tests/run_tests.sh
+++ src/tests/run_tests.sh
@@ -5,5 +5,5 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-cd $DIR
+cd "$DIR"
 python -m unittest -v audiofile_tests
